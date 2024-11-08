a5718f1629031d164e0b5e66255d0c7043183ddb
Mikheil17 <mikheil.gabrielidze@gmail.com>
2024-11-08 16:21:52 -0600

    Update cart.py
    Cart class basic initialization

--- cart.py
+++ cart.py
@@ -1,4 +1,18 @@
+import sqlite3
+
 class Cart:
 
-    def __init__(self):
-        sample = ""
+    def __init__(self, databaseName="methods.db"):
+        pass  # Placeholder for initialization code
+
+    def viewCart(self, userID):
+        pass  # Placeholder for code to view cart
+
+    def addToCart(self, userID, ISBN, quantity):
+        pass  # Placeholder for code to add item to cart
+
+    def removeFromCart(self, userID, ISBN, quantity):
+        pass  # Placeholder for code to remove item from cart
+
+    def checkout(self, userID):
+        pass  # Placeholder for code to checkout
