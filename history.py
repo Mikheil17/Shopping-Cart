6c9082ea1d5458822a4740c2222431ef8ddc0eb2
AUGDoktor <42jdok@gmail.com>
2024-11-08 18:19:27 -0600

    added initial functions for OrderHistory

--- history.py
+++ history.py
@@ -1,4 +1,18 @@
+import sqlite3
+
 class OrderHistory:
 
-    def __init__(self):
-        sample = ""
+    def __init__(self, databaseName="methods.db"):
+        self.databaseName = databaseName
+    
+    def viewHistory(self, userID):
+        pass
+    
+    def viewOrder(self, userID, orderID):
+        pass
+    
+    def createOrder(self, userID, quantity, cost, date):
+        pass
+    
+    def addOrderItems(self, userID, orderID):
+        pass
